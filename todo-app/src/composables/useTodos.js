b14a44adfa203f4ef1bea54d2386f8cf05f0a672
kangbo <8431331654@qq.com>
2021-02-04 14:34:36 +0800

    自动发布

--- todo-app/src/composables/useTodos.js
+++ todo-app/src/composables/useTodos.js
@@ -4,7 +4,6 @@ export default function useTodos() {
   const todos = ref([]);
   // 添加 todo
   const addTodo = (todo) => todos.value.push(todo);
-  console.log(addTodo)
   // 获取远程 todos
   const fetchTodos = async () => {
     const response = await fetch(
