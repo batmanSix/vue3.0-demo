50937ad4ff7828e15c28638a9f8bbc26066aa91b
kangbo <8431331654@qq.com>
2021-02-05 11:19:02 +0800

    提交修改

--- src/composables/useTodos.js
+++ src/composables/useTodos.js
@@ -6,15 +6,16 @@ export default function useTodos() {
   const addTodo = (todo) => todos.value.push(todo);
   // 获取远程 todos
   const fetchTodos = async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/todos?_limit=5"
-    );
-    const rawTodos = await response.json();
-    todos.value = rawTodos.map((todo) => ({
-      id: todo.id,
-      content: todo.title,
-      completed: todo.completed,
-    }));
+    // const response = await fetch(
+    //   "https://jsonplaceholder.typicode.com/todos?_limit=5"
+    // );
+    // const rawTodos = await response.json();
+    // // console.log(rawTodos)
+    // // todos.value = rawTodos.map((todo) => ({
+    // //   id: todo.id,
+    // //   content: todo.title,
+    // //   completed: todo.completed,
+    // // }));
   };
 
   onMounted(() => {
