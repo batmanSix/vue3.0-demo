10d5fb505346dcb44308837c4a783014bd4aafe1
kangbo <8431331654@qq.com>
2021-04-06 23:11:23 +0800

    add

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,9 @@ module.exports = {
     env: {
       browser: true,
     },
-    extends: [
-      // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-      // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-      'plugin:vue/essential', 
-      // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-      'standard'
+    "extends": [
+      "eslint:recommended",
+      "plugin:vue/recommended"
     ],
     // required to lint *.vue files
     plugins: [
